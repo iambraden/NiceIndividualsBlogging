6d31b1fe7475db7ec5f75aeda880239f83929513
iambraden <iambradencollins@outlook.com>
2025-03-16 13:40:44 -0700

    rudimentary authenticate file

---
+++ php/authenticate.php
@@ -0,0 +1,26 @@
+<?php
+error_reporting(E_ALL); 
+ini_set('display_errors', 1);
+session_start();
+
+//Database not set up yet. Hardcoded user and password.
+$validUser = [
+    "testUser" => "testPassword",
+    "testUser2" => "testPassword2" 
+];
+
+$username = $_POST['username'] ?? '';
+$password = $_POST['password'] ?? '';
+$role = $_POST['role'] ?? '';
+
+//Check if user is valid
+if(isset($validUser[$username]) && $validUser[$username] == $password){
+    $_SESSION['username'] = $username;
+    $_SESSION['role'] = $role;
+    header('Location: ../profile.html');
+    exit();
+}else{
+    header('Location: ../signin.html');
+    exit();
+}
+?>
