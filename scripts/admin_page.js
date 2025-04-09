17b0eb4f0e19b162aee6830ed7119925f7801518
jcroze <jayjac2020@gmail.com>
2025-04-08 20:32:53 -0700

    Adding Search by Email
    Admin can now change a user's role based on their email.
    This branch also fixes validation issues.

--- scripts/admin_page.js
+++ scripts/admin_page.js
@@ -2,43 +2,39 @@ document.addEventListener('DOMContentLoaded', function(){
     const form = document.querySelector('form');
     
     const userName = document.getElementById('selectUsername');
+    const userByEmail = document.getElementById('selectUserByEmail');
     const radios = document.getElementsByName("user_role");
 
     const userReq = userName.insertAdjacentElement('afterend', document.createElement('div'));
 
     userReq.classList.add('required');
 
-    userReq.innerHTML = 'Username is required';
-
     userName.innerHTML = 'Select an option';
-
+    
     let isChecked = false;
+    let isFilled = false;
 
     userName.addEventListener('input', function(){
-        if(userName.value.trim() === ''){
-            userReq.innerHTML = 'Username is required';
+        if(userName.value.trim() === '' && userByEmail.value.trim() === ''){
+            userReq.innerHTML = 'A username or user\'s email is required';
+            isFilled = false;
         }else{
             userReq.innerHTML = '';
+            isFilled = true;
         }
     });
 
-    radios.addEventListener('input', function(){
-        for (let i = 0; i < radios.length; i++) {
-            if (radios[i].checked) {
-                isChecked = true;
-                break;
-            }
-        }
-        if (isChecked === false){
-            userName.innerHTML = 'Select an option';
-        }
+    radios.forEach(function (radio) {
+        radio.addEventListener('input', function () {
+            isChecked = Array.from(radios).some(radio => radio.checked);
+        });
     });
 
     form.addEventListener('submit', function(event){
        
-        if(userName.value.trim() === '' || isChecked === false){
+        if(isFilled === false || isChecked === false){
             event.preventDefault();
-            alert('Please fill in all fields');
+            alert('Please fill in required fields');
         }
         // If all fields are filled, the form will submit to the server
     });
